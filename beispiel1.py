fcefcdd7817894a61c0c7a5ac6b2a28c918731d7
nues666 <171wita21@o365.htl-leoben.at>
2018-06-04 16:42:12 +0200

    asjklf

--- beispiel1.py
+++ beispiel1.py
@@ -1,6 +1,32 @@
+#beispiel1.py
+def pruefziffer(code):
+    check = (98-((int(code))%97))
+    return check
+
 land = "AT"
 blz = "38282"
 konto = "3026713"
-check = "96"
 
-print(land + " "+blz+" "+ konto+" "+ check)
+kontolen = 11
+
+
+if(len(konto) != kontolen):
+    difference = kontolen-len(konto)
+    for i in range(len(konto), kontolen):
+        konto = "0" + konto
+numbers = blz+konto
+counter = 0
+iban=blz+konto+"1029"+"00"
+check = pruefziffer(iban)
+print(check)
+print(land + str( check), end = " ")
+
+
+
+for i in range(0, len(numbers)):
+    counter += 1
+    print(numbers[i], end = "")
+    if(counter == 4):
+        print(" ", end = "")
+        counter = 0
+
